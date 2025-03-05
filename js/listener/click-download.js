0ecc3ee7c118178fc8629ffddd200679d8e75e96
Shirley Lu <contact@shirleylu.com>
2025-03-05 01:30:02 -0800

    adjust fonts and css

--- js/listener/click-download.js
+++ js/listener/click-download.js
@@ -1,18 +1,12 @@
 function makeAndDownload() {
-  // Use html2canvas to capture the div
-  html2canvas(document.getElementById("card"), {
-    allowTaint: false,
-    useCORS: true,
-    logging: true
-
+  const card = document.getElementById("card");
+  html2canvas(card, {
+    useCORS: true
   }).then(function(canvas) {
-    // Convert the canvas to a data URL (image format)
-    var imgData = canvas.toDataURL("image/png");
-
-    // Create an anchor element to trigger the download
-    var link = document.createElement('a');
-    link.href = imgData;
-    link.download = "card-image.png"; // Set the filename for the image
-    link.click(); // Trigger the download
+    const imageData = canvas.toDataURL('image/png');
+    const link = document.createElement("a");
+    link.href = imageData;
+    link.download = "card.png";
+    link.click();
   })
 }
